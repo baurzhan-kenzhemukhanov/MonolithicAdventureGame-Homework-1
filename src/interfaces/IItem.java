66703039435789bb2ef2123e97fe897179b2248c
baurzhan-kenzhemukhanov <baurzhan.kenzhemukhanov@narxoz.kz>
2025-02-10 20:36:53 +0500

    Interface enumeration

---
+++ src/interfaces/IItem.java
@@ -0,0 +1,12 @@
+package interfaces;
+
+public interface IItem extends IEntity {
+    void use(ICombatant target);
+    IItemType.ItemType getItemType();
+}
+
+enum ItemType {
+    HEALTH_POTION,
+    EXPERIENCE_SCROLL,
+    GOLD
+} 
