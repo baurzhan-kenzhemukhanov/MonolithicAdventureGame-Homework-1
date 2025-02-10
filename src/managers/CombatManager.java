7a844c847852a5268a2a1197f1d581131c63bc05
baurzhan-kenzhemukhanov <baurzhan.kenzhemukhanov@narxoz.kz>
2025-02-10 21:51:36 +0500

    HW

--- src/managers/CombatManager.java
+++ src/managers/CombatManager.java
@@ -6,14 +6,12 @@ public class CombatManager {
     public void processCombat(ICombatant attacker, ICombatant defender) {
         System.out.println(attacker.getName() + " engages in combat with " + defender.getName() + "!");
         
-        // Combat round
         int damage = attacker.dealDamage();
         defender.takeDamage(damage);
         
         System.out.println(attacker.getName() + " deals " + damage + " damage to " + defender.getName());
         System.out.println(defender.getName() + " health: " + defender.getHealth());
         
-        // If defender is still alive, they counter-attack
         if (defender.getHealth() > 0) {
             damage = defender.dealDamage();
             attacker.takeDamage(damage);
