8fc60b2901f242a3352b9a08175295bd0f65ef6f
baurzhan-kenzhemukhanov <baurzhan.kenzhemukhanov@narxoz.kz>
2025-02-10 22:49:38 +0500

    HW

--- src/managers/CombatManager.java
+++ src/managers/CombatManager.java
@@ -3,20 +3,23 @@ package managers;
 import interfaces.ICombatant;
 
 public class CombatManager {
+
     public void processCombat(ICombatant attacker, ICombatant defender) {
         System.out.println(attacker.getName() + " engages in combat with " + defender.getName() + "!");
-        
-        int damage = attacker.dealDamage();
-        defender.takeDamage(damage);
-        
-        System.out.println(attacker.getName() + " deals " + damage + " damage to " + defender.getName());
-        System.out.println(defender.getName() + " health: " + defender.getHealth());
-        
+
+        executeAttack(attacker, defender);
+
         if (defender.getHealth() > 0) {
-            damage = defender.dealDamage();
-            attacker.takeDamage(damage);
-            System.out.println(defender.getName() + " counter-attacks for " + damage + " damage");
-            System.out.println(attacker.getName() + " health: " + attacker.getHealth());
+            executeAttack(defender, attacker);
         }
     }
-} 
+
+    private void executeAttack(ICombatant attacker, ICombatant defender) {
+        int damage = attacker.dealDamage();
+        defender.takeDamage(damage);
+
+        System.out.println(attacker.getName() + " deals " + damage + " damage to " + defender.getName());
+        System.out.println(defender.getName() + " health: " + defender.getHealth());
+    }
+}
+
